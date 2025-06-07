3c2e527b70ebb21334ae6f4989d59e25d7c7c3b5
Rayssa Silva <rayssalemescrz05@outlook.com>
2025-06-07 17:24:47 -0300

    Corrigindo componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Sidebar from './components/sidebar'
+import Sidebar from './components/Sidebar'
 import MainContent from './components/MainContent'
 
 import './styles/components/app.sass'
